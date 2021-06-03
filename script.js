dbef101d3f59495587c1dc20eb55b355d7314a6f
Vlada Plutalova <vladaplutalova@yandex.ru>
2021-06-03 16:58:56 +0300

    feat: added image and sound

--- script.js
+++ script.js
@@ -3,16 +3,24 @@
   console.log(e.code);
 });*/
 
-
-const keys = document.querySelectorAll('.piano-key'); /* получаем все кнопки пианино */ 
+/* keys - массив с кнопками */
+const keys =
+  document.querySelectorAll(".piano-key"); /* получаем все кнопки пианино */
 /* потом вешаем событие клика на кнопки */
-    keys.forEach(piano-key => {
-    piano-key.addEventListener('click', playNote); 
+keys.forEach((key) => {
+  /* в массиве для каждого его элемента key при клике срабатывает ф-ия playNote*/
+  key.addEventListener("click", playNote);
 });
 
-function playNote(e){
-    /* нужно узнать по какой клавише кликнули.
-    Свойство target является ссылкой на объект, который был инициатором события*/ 
-     let key = e.target;
-     key.classList.add('active');
+function playNote(e) {
+  /* нужно узнать по какой клавише кликнули.
+    Свойство target является ссылкой на объект, который был инициатором события*/
+  let key = e.target;
+  let sound = document.getElementById(key.dataset.note);
+  key.classList.add("active"); /* добавляем класс эктив - красим клавишу в голубой цвет */
+  sound.currentTime = 0;
+  sound.play();
+  sound.addEventListener("ended", () => {
+    key.classList.remove("active");
+  });
 }
