540a4aabbe190e9b429724ed433282f53a4bc6f0
Vlada Plutalova <vladaplutalova@yandex.ru>
2021-06-04 16:10:30 +0300

    feat: play only note C for all buttons

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-/*window.addEventListener("keydown", function (e) {
-  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-  console.log(e.code);
-});*/
+/* SOUND - CLICK */
 
 /* keys - массив с кнопками */
 const keys =
@@ -9,18 +6,47 @@ const keys =
 /* потом вешаем событие клика на кнопки */
 keys.forEach((key) => {
   /* в массиве для каждого его элемента key при клике срабатывает ф-ия playNote*/
-  key.addEventListener("click", playNote);
+  key.addEventListener("click", playAudio);
 });
 
-function playNote(e) {
+function playAudio(event) {
   /* нужно узнать по какой клавише кликнули.
     Свойство target является ссылкой на объект, который был инициатором события*/
-  let key = e.target;
+  let key = event.target;
   let sound = document.getElementById(key.dataset.note);
-  key.classList.add("active"); /* добавляем класс эктив - красим клавишу в голубой цвет */
+  key.classList.add(
+    "active"
+  ); /* добавляем класс эктив - красим клавишу в голубой цвет */
   sound.currentTime = 0;
   sound.play();
   sound.addEventListener("ended", () => {
     key.classList.remove("active");
   });
 }
+
+/* SOUND - KEYDOWN */
+
+/*const pianoKeys = document.querySelectorAll(".piano-key");
+
+pianoKeys.forEach((pianoKey) => {
+  pianoKey.addEventListener('keydown', playNote);
+});*/
+
+/*function playNote(event) {
+  let pianoKey = event.target;
+  let note = document.getElementById(pianoKey.dataset.note);
+  pianoKey.classList.add("active"); 
+  note.currentTime = 0;
+  note.play();
+  note.addEventListener("ended", () => {
+    pianoKey.classList.remove("active");
+  });
+}*/
+
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-keyNote="68"]`);
+  if (!audio) return;
+  audio.play();
+});
+
+/* ADD BUTTON LETTER/NOTE */
